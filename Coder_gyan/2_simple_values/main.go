b4a8ffda1fd40edbf40c2239ada3fa6c71e6eaf5
Sudhanshu-06 <sudhanshu767778@gmail.com>
2024-11-03 21:39:15 +0530

    done

---
+++ Coder_gyan/2_simple_values/main.go
@@ -0,0 +1,29 @@
+package main
+import "fmt"
+
+func main(){
+	// simple values
+	// int
+	fmt.Println(1)
+	fmt.Println(1+1)
+
+	// string
+	fmt.Println("Hello, World!")
+
+    // bool
+	fmt.Println(true)
+	fmt.Println(false)
+
+	// floats
+	fmt.Println(3.14)
+    fmt.Println(3.14 + 3.14)
+	fmt.Println(7.0/3.0)
+
+    // complex numbers
+    fmt.Println(2 + 3i)
+    fmt.Println(2 + 3i + 2 + 3i)
+
+    
+    
+
+}
