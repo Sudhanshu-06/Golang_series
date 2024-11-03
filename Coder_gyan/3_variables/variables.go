26414de8814194ccca07df26e3012bf551e34f13
Sudhanshu-06 <sudhanshu767778@gmail.com>
2024-11-03 21:52:47 +0530

    done

---
+++ Coder_gyan/3_variables/variables.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var name string = "Sudhanshu"
+
+	// imfer
+	// var name="Sudhanshu"
+	// var isAdult bool=true
+
+	// var age int =24
+
+// short hand suntax
+    // name := "Sudhanshu"
+    // isAdult := true
+    // age := 24
+
+
+	// fmt.Println(name,isAdult,age)
+
+	// var name string
+	// name="Aman"
+	// fmt.Println(name)
+
+}
