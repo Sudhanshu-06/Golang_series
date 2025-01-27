9b28be722b604444b189af61832a1d7d970d7384
Sudhanshu-06 <sudhanshu767778@gmail.com>
2025-01-27 11:37:08 +0530

    done

---
+++ Coder_gyan/8_arrays/arrays.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+// numbered squence of specific length
+func main() {
+	// zerod values
+	// innt-0 string-empty string boolean- false
+
+	// var nums[4]int
+	// nums[0] = 1
+
+
+	// fmt.Println(nums[0])
+	// fmt.Println(nums)
+	// array length
+	// fmt.Println(len(nums))
+
+
+	// var vals[4]bool
+	// vals[2]=true
+	// fmt.Println(vals)
+
+
+	var name[3]string
+	name[0]="golang"
+	fmt.Println(name)
+
+//  to declare in single line format
+	nums :=[3]int{1,2,3}
+	fmt.Println(nums)
+
+	// 2d arrays
+	nums1 :=[2][2]int{{1,2,},{4,5,}}
+	fmt.Println(nums1)
+}
